33eda5ed6aa4d762fc985624e2ce4265071e395a
HALISH RICHARD J <90974013+halishrichard5@users.noreply.github.com>
2022-11-30 05:56:47 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,49 @@
+package ATM;
+
+/**
+ *
+ * @author HALISH RICHARD J
+ */
+import OptionMenu.OptionMenu;
+import java.io.IOException;
+public class ATM extends OptionMenu{
+
+
+ 
+
+	public static void main(String[] args) throws IOException {
+		OptionMenu optionMenu = new OptionMenu();
+	
+		optionMenu.mainMenu();
+	}
+}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ReadME.txt
@@ -0,0 +1,8 @@
+step1:
+	to be compil "OptionMenu.java"         ---(javac [File_Name] )
+step2:
+	then compil "Account.java"	      ---(javac [File_Name] )
+step3:
+	then compail "ATM.java"		      ---(javac [File_Name] )
+step4: 
+        then to run the "ATM"	              ---(java [File_Name] )
